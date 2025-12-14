c410f0e4fd4189c6d65aca3233eb1f955504f711
an1skat <156462349+an1skat@users.noreply.github.com>
2025-12-14 12:45:27 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-	origin: ["http://localhost:5173/", "https://24forecast.netlify.app/"],
+	origin: ["http://localhost:5173", "https://24forecast.netlify.app"],
 	credentials: true,
 }))
 
